e2b091c20ec73f256d0294000754a4e0b52409fb
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2023-10-07 17:36:04 -0300

    Criação do serviço de autorização

---
+++ autenticacao_autorizacao_api/src/main/java/com/example/demo/config/security/SecurityConfigurations.java
@@ -0,0 +1,10 @@
+package com.example.demo.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+}
